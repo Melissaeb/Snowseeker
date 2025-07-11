5f70477215bb527447a2a2e1e09a26c7ec8df3b6
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2025-07-11 13:26:03 +1200

    Initial commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Melissa Elliston-Boyes on 11/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
